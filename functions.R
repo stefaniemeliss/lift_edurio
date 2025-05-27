d409928f22f6ebb58bc45a958d19b40ecc750cb7
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-05-27 13:30:53 +0100

    Create functions.R

---
+++ functions.R
@@ -0,0 +1,13 @@
+#### ALL-PURPOSE HELPER FUNCTIONS ####
+
+# source code
+source_code(target_repo = "helper_functions", file_name = "functions.R")
+
+#### PROJECT-SPECIFIC FUNCTIONS ####
+
+# functions for data processing #
+
+# Function to insert "/" in the format "YYYYYY" to "YYYY/YY"
+insert_slash <- function(number) {
+  sub("(\\d{4})(\\d{2})", "\\1/\\2", number)
+}
